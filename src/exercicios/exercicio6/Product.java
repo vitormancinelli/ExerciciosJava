04b2b563ed5b5dd0416408f0c6196d5a616ce8e3
Vitor Mancinelli <vitormancinelli@gmail.com>
2024-06-23 12:23:15 -0300

    Detalhamento Exercicio 6

---
+++ src/exercicios/exercicio6/Product.java
@@ -0,0 +1,59 @@
+package exercicios.exercicio6;
+
+/**
+ * Essa classe tem como objetivo armazenar as informações do Produto
+ * 
+ * Essa classe deve ter a estrutura do Objeto Produto, para depois ser utilizado no sistema
+ * 
+ * @author vitor
+ *
+ */
+public class Product {
+
+	//Variável utilizada para armazenar o valor do código do produto
+	//Deve ser publica para poder ser acessada de fora dessa classe, utilizando a seguinte forma: nome da variavel + .codigo
+	//Exemplo: Product produto = new Product();
+	//produto.codigo = 1;
+	//System.out.println("Codigo do produto = " + produto.codigo)
+	public Integer codigo;
+	
+	//Variável utilizada para armazenar o valor do nome do produto
+	//Deve ser publica para poder ser acessada de fora dessa classe, utilizando a seguinte forma: nome da variavel + .nome
+	//Exemplo: Product produto = new Product();
+	//produto.nome = "banana";
+	//System.out.println("Nome do produto = " + produto.nome)
+	public String nome;
+	
+	//Variável utilizada para armazenar o valor do valor do produto
+	//Deve ser publica para poder ser acessada de fora dessa classe, utilizando a seguinte forma: nome da variavel + .valor
+	//Exemplo: Product produto = new Product();
+	//produto.valor = 10;
+	//System.out.println("Valor do produto = " + produto.valor)
+	public Integer valor;
+	
+	//Variável utilizada para armazenar o valor do código do produto
+	//Deve ser publica para poder ser acessada de fora dessa classe, utilizando a seguinte forma: nome da variavel + .tipo
+	//Exemplo: Product produto = new Product();
+	//produto.tipo = "fruta";
+	//System.out.println("Codigo do produto = " + produto.tipo)
+	public String tipo;
+	
+	//Método construtor padrão, onde não é recebido nenhum valor, porém por padrões de código não colocamos nenhum valor nas váriaveis, retornado todos as váriaveis do Objeto sem valor
+	public Product() {
+		
+	}
+	
+	//Método construtor com valor para todas váriaveis
+	//Por padrão de código normalmente utilizamos o mesmo nome das váriaveis da classe
+	//Para que seja possível apontarmos no código que queremos colocar o valor da váriavel recebida no método na várivel da classe é necessário utilizarmos o "this."
+	//O uso do "this." é necessário para indicarmos que queremos acessar algo dentro da Classe e não utilizarmos a váriavel local do método
+	//Dessa forma estamos pegando os valores recebidos no método e carregando-os dentro das váriaveis da classe
+	//Por isso ao utilizar essa método construtor a instancia dessa classe já vai ter os valores carregados nas váriaveis
+	public Product(Integer codigo, String nome, Integer valor, String tipo) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.valor = valor;
+		this.tipo = tipo;
+	}
+	
+}
--- src/exercicios/exercicio6/ProductRegister.java
+++ src/exercicios/exercicio6/ProductRegister.java
@@ -1,5 +1,45 @@
 package exercicios.exercicio6;
 
+/**
+ * 
+ * Implementar os metodos abaixo como orientado de forma a completar o objetivo dessa tarefa.
+ * 
+ * Ao iniciar o sistema deverá ser realizado as perguntas para receber os valores do Produto a ser Criado
+ * 
+ * Esse sistema não precisará de menu, pois sua execução será simples, tendo como foco a criação de Objetos.
+ * 
+ * @author vitor
+ *
+ */
 public class ProductRegister {
 
+	/**
+	 * Quando finalizar a implementação dos metodos, execute esse metodo para testar a implementação
+	 */
+	public static void main(String[] args) {
+		//Utilizar a classe Scanner para ler os valores que o usuário digitar no console
+		//Os valores inseridos pelo usuário serão utilizados para criar o Produto
+		//Os valores necessários serão:
+		//Codigo -> Numerico
+		//Nome -> Texto
+		//Valor -> Numerico
+		//Tipo -> Texto
+		
+		//OBS: Caso não tenha conhecimento sobre Objetos em Java, pesquise, porém segue uma breve explicação sobre sua estrutura.
+		//--- Java é uma linguagem orientada a objetos, ou seja os fluxos dos sistemas criados em Java devem ser focados em Objetos e não a processos
+		//--- Objeto normalmente é uma classe que deve ter váriaveis contendo a estrutura dos valores que serão armazenados no mesmo.
+		//--- Para utilizar um Objeto é necessário criar uma instancia do Objeto, dessa forma você cria uma váriavel contendo a estrutura do Objeto, porém sem nenhum valor dessa forma você pode ter varias intancias do mesmo Objeto sem interferir um no outro
+		//--- Para criar uma instancia de um Objeto é necessário utilizar um método construtor para gerar a estrutura do Objeto, new + Nome da Classe + Valores de Entrada do Método, Exemplo: new Product()
+		//OBS: Para criar uma instancia de um Objeto é similar a criação de uma váriavel normal, Nome do Objeto + Nome da Variavel. Exemplo: Product primeiroProduto = new Product();
+		//OBS: Por padrão toda classe tem um construtor que não recebe nenhum valor de entrada e retorna a estrutura do Objeto sem nenhum valor preenchido, porém podemos sobrescrever o construtor padrão apenas criando um novo construtor
+		//--- No caso de criação do produto temos duas forma comuns de criar carregar os valores no Produto criado, Exemplo 1: new Product(1, "banana", 10, "fruta");
+		//--- Exemplo 2: Product primeiroProduto = new Product(); 
+		//--- primeiroProduto.codigo = 1;
+		//--- primeiroProduto.nome = "banana";
+		//--- primeiroProduto.valor = 10;
+		//--- primeiroProduto.tipo = "fruta";
+		//OBS: Ambos os exemplos tem o mesmo resultado, PORÉM para que o Exemplo 1 seja possível é ncessário criar um construtor para receber os 4 valores
+		//--- Para facilitar o entendimento consulte a classe Product dentro do pacote "exercicio6" onde irá ter um construtor padrão e um construtor para o Exemplo 1
+	}
+	
 }
