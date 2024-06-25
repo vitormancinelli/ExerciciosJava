b1a9b92f657f4f4606ab2020d6a6e6cfea4571fe
guimatrone0 <ggmatrone@gmail.com>
2024-06-25 18:50:52 -0300

    Alterando exercicio 8

---
+++ src/exercicios/exercicio8/DataBaseConnection.java
@@ -0,0 +1,33 @@
+package exercicios.exercicio8;
+
+public class DataBaseConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/** 
+ * 	try {
+ * 		Class.forName("com.mysql,jdbc,Driver");
+ * 	 metodo (Class.forName)utilizado para carregar o driver do banco de dados para nossa aplicação 
+ *  este metodo espera uma string que seria o caimnho completo da classe de driver do meu banco de dados
+ *  agr vamos criar a conexão usando a classe conection do pacote java sql
+ *  
+ *  Connetion nomedaconexao = DriverManager.getConnetion("url", "user", "password")
+ *  aa classe DriverManager serve para instaciar e criar a getConnetion(
+ * este metodo espera 3 parametros (url, user, password), que em url deve ser 
+ * ("jdbc:mysql://ipoulocalhost/nome do banco)
+ * 
+ * } catch (ClassNotFoundException ex) {
+ *  	System.out.println("O driver do banco de dados nao pode ser localizado");
+ * 	esse procedimento é realizado devido a condição de caso o metodo não encontre o caminho 
+ * para o banco de dados ou o banco n exista
+ * } catc (SQLException ex) {
+ * 	ystem.out.println("ocorreu um erro ao acessar o banco") + ex.getMessage());
+ * 
+ * 
+ * 		}
+ * 
+ * 
+ */
+	}
+}
+//fazer classes para a lista de produtos e criar as services para adicionar 
